eeac74d52a3dcc075714908c717e3817d01f80b7
Luiz Fernando <lfaires@gmail.com>
2021-04-20 11:38:43 -0300

    script axios/ionicon adicionado e função para buscar os quizzes

--- script/screen1.js
+++ script/screen1.js
@@ -1,6 +1,33 @@
+getQuizzes()
+
 function openCreateQuizz(){
     const firstScreen = document.querySelector(".first-screen")
     const thirdScreen = document.querySelector(".third-screen")
     firstScreen.classList.add("hide")
     thirdScreen.classList.remove("hide")
+}
+
+function getQuizzes() {
+    const promiseQuizz = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/buzzquizz/quizzes")
+
+    promiseQuizz.then(displayQuizz)
+    promiseQuizz.catch(errorDisplayQuizz)
+}
+
+function displayQuizz(resposta) {
+    const ulQuizzes = document.querySelector(".quizzes")
+    const quizzes = resposta.data
+    
+    for(let i=0;i < quizzes.length;i++){
+        let quizz = quizzes[i];
+        ulQuizzes.innerHTML += `
+        <li id="Q-${quizz.id}"><div class="quizz-title">${quizz.title}</div></li>
+        `
+        document.getElementById(`Q-${quizz.id}`).style.backgroundImage = `linear-gradient(#FFFFFF00, #00000080),url(${quizz.image})`
+    }
+}
+
+function errorDisplayQuizz(resposta) {
+    window.location.reload()
+    getQuizzes()
 }
