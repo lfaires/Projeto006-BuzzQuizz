2a50bf938db84ae0da8f93303303ba3f93816866
Luiz Fernando <lfaires@gmail.com>
2021-04-21 18:32:55 -0300

    criação dos campos de pergunta e niveis pelo javascript

--- script/screen3.js
+++ script/screen3.js
@@ -1,6 +1,14 @@
-let isValidationOk = false;
+let isValidationQuizzOk = false;
+let isValidationQuestionOk = false;
+let isValidationLevelOk = false;
+const createStart = document.querySelector(".create-start")
+const createQuestion = document.querySelector(".create-questions")
+const createLevel = document.querySelector(".create-levels")
+const createReady = document.querySelector(".create-ready")
 
-function createQuizz(){
+
+//FUNÇÕES PARA SUBMETER
+function submitQuizz(){
     alert()
     const titleQuizz = document.querySelector("#quizz-title").value
     const imageQuizz = document.querySelector("#quizz-image").value
@@ -10,25 +18,79 @@ function createQuizz(){
     const valid = validationQuizz(titleQuizz, imageQuizz, numberQuestions, numberLevels)
     
     if (valid === true) {
-        console.log("deu certo")
+        clearStartQuizz()
+        createQuestionAndLevel(numberQuestions, numberLevels)
+        return alert("validação ok")
         //Abrir tela de criação de perguntas
     } else {
-        console.log("Deu ruim")
-        return alert("Preencheu algo errado")
+        return alert("Por favor, preencha os dados corretamente!")
     }
 }
 
-function clearStarQuizz(){
+function submitQuestion(){
+    //validação dos dados das perguntas
+    createQuestion.classList.add("hide")
+    createLevel.classList.remove("hide")
+}
+
+function submitLevel(){
+        //validação dos dados dos níveis
+    createLevel.classList.add("hide")
+    createReady.classList.remove("hide")
+}
+
+//FUNÇÃO PARA INSERIR A QUANTIDADE DE PERGUNTAS E NÍVEIS
+
+function createQuestionAndLevel(numeroQuestoes, numeroNiveis){
+    createStart.classList.add("hide")
+    createQuestion.classList.remove("hide")
+    for (let i=0; i<numeroQuestoes; i++){
+        createQuestion.innerHTML += `
+        <li class="question-${i+1}">
+            <div class="heading">Pergunta ${i+1}</div>
+                <input class="" placeholder="Texto da pergunta">
+                <input class="" placeholder="Cor de fundo da pergunta">
+            <div class="heading">Resposta correta</div>
+                <input class="" placeholder="Resposta correta">
+                <input class="" placeholder="URL da imagem">
+            <div class="heading">Respostas incorretas</div>
+                <input class="" placeholder="Resposta incorreta 1">
+                <input class="" placeholder="URL da imagem 1">
+                <input class="" placeholder="Resposta incorreta 2">
+                <input class="" placeholder="URL da imagem 2">
+                <input class="" placeholder="Resposta incorreta 3">
+                <input class="" placeholder="URL da imagem 3">
+        </li>`
+    }
+    createQuestion.innerHTML += `<button onclick="submitQuestion()">Prosseguir pra criar níveis</button>`
+
+    for (let i=0; i<numeroNiveis; i++){
+        createLevel.innerHTML += `
+        <li class="level-${i+1}">
+            <div class="heading">Nível ${i+1}</div>
+            <input class="" placeholder="Título do Nível">
+            <input class="" placeholder="% de acerto mínima">
+            <input class="" placeholder="URL da imagem do nível">
+            <input class="" placeholder="Descrição do nível">
+        </li>`
+    }
+    createLevel.innerHTML += `<button onclick="submitLevel()">Finalizar Quizz</button>`
+}
+// FUNÇÃO PARA LIMPAR OS INPUT DA TELA 3.1
+
+function clearStartQuizz(){
     document.querySelector("#quizz-title").value = ""
     document.querySelector("#quizz-image").value = ""
     document.querySelector("#quizz-questions").value = ""
     document.querySelector("#quizz-levels").value = ""
 }
 
+// FUNÇÕES DE VALIDAÇÃO
+
 function validationQuizz(title, url, questions, levels) {
-    if((title.length >= 20 && title.length <= 68) || (isNaN(questions) === false && questions >= 3) || (isNaN(levels) === false && levels >= 2) ){
-        isValidationOk = true
-        return isValidationOk
+    if((title.length >= 20 && title.length <= 68) && (isNaN(questions) === false && questions >= 3) && (isNaN(levels) === false && levels >= 2) ){
+        isValidationQuizzOk  = true
+        return isValidationQuizzOk 
     }
     //validação da url ainda não está feita
     /*if(url) === false && numberQuestions >= 2){
@@ -37,3 +99,7 @@ function validationQuizz(title, url, questions, levels) {
         validTeste.push(false)
     }*/
 }
+
+//function validationQuestion(){}
+
+//function validationLevel(){}
