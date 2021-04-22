68e13eaf7d216e7efff4ff2cbe0a6c410468a277
Luiz Fernando <lfaires@gmail.com>
2021-04-22 15:43:39 -0300

    inicio da função das perguntas do quizz

--- script/screen1.js
+++ script/screen1.js
@@ -1,10 +1,11 @@
+const firstScreen = document.querySelector(".first-screen")
+const secondScreen = document.querySelector(".second-screen")
+const thirdScreen = document.querySelector(".third-screen")
 let iD = [];
 let myId = []
 getQuizzes()
 
 function openCreateQuizz(){
-    const firstScreen = document.querySelector(".first-screen")
-    const thirdScreen = document.querySelector(".third-screen")
     firstScreen.classList.add("hide")
     thirdScreen.classList.remove("hide")
 }
@@ -29,9 +30,19 @@ function displayQuizz(resposta) {
         <li id="Q-${quizz.id}" onclick="goToQuizz(${quizz.id})"><div class="quizz-title">${quizz.title}</div></li>
         `
         document.getElementById(`Q-${quizz.id}`).style.backgroundImage = `linear-gradient(#FFFFFF00, #00000080),url(${quizz.image})`
+
+        //Populando os quizzes na screen 2
+        secondScreen.innerHTML += `
+        <div class="Q-${quizz.id} hide">
+            <div class="quizz-top"><span>${quizz.title}</span></div>
+            <ul class="quizz-questions"></ul>
+        </div>`;
+    
     }
     const allId = checkID(iD)
 }
+
+
 function errorDisplayQuizz(resposta) {
     window.location.reload()
     getQuizzes()
@@ -50,4 +61,18 @@ function displayMyQuizz() {
     const createQuizz = document.querySelector(".create-quizz")
     createQuizz.classList.add("hide")
     myQuizz.classList.remove("hide")
+}
+
+//clicar no quiz e redirecionar pra tela 2
+
+function goToQuizz(idQuizz){
+    alert("Q-"+idQuizz)
+    firstScreen.classList.add("hide")
+    secondScreen.classList.remove("hide")
+    const quizzScreen = document.querySelector(".second-screen .Q-"+idQuizz)
+    quizzScreen.classList.remove("hide")
+}
+
+function inputQuestions(arrayObjetos){
+   
 }
